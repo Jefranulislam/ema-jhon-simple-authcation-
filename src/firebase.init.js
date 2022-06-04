52923062d789fe643f46f0a8afcac6cffcb76cdb
JefranulIslam <jefranulislam@gmail.com>
2022-06-04 23:42:07 +0600

    initiial react setup

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCmKibfvFAnXC_aq3V6wPNf29TaXcviwKo",
+    authDomain: "ema-john-simple-31c4d.firebaseapp.com",
+    projectId: "ema-john-simple-31c4d",
+    storageBucket: "ema-john-simple-31c4d.appspot.com",
+    messagingSenderId: "1014139545608",
+    appId: "1:1014139545608:web:61694d2b87af6cd8ea89bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
